40ad008c065eb7e99682f0ad9a77e6a151f77d23
Moises Borges <moises.borges@gmail.com>
2019-01-30 06:28:03 -0400

    Setup DI framework

--- app/src/main/java/net/moisesborges/App.kt
+++ app/src/main/java/net/moisesborges/App.kt
@@ -1,10 +1,13 @@
 package net.moisesborges
 
 import android.app.Application
+import net.moisesborges.di.appModule
+import org.koin.android.ext.android.startKoin
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin(this, listOf(appModule))
     }
 }
