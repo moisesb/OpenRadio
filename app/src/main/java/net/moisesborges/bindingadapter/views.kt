7c321f5e2d1ede47e7084ac7ccedced409957f80
Moises Borges <moises.borges@gmail.com>
2019-02-16 06:41:22 -0400

    Improve Top Stations RecyclerView items UI

--- app/src/main/java/net/moisesborges/bindingadapter/views.kt
+++ app/src/main/java/net/moisesborges/bindingadapter/views.kt
@@ -3,7 +3,7 @@ package net.moisesborges.bindingadapter
 import android.view.View
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter("android:setVisibility")
+@BindingAdapter("setVisibility")
 fun setVisibilityBinding(view: View, isVisible: Boolean) {
     view.visibility = if (isVisible) View.VISIBLE else View.GONE
 }
