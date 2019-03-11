b5b45e0500537d12246130e15463f94c3b08cdb8
Moisés Borges dos Anjos <moises.borges@gmail.com>
2019-03-11 05:46:35 -0300

    Top stations pagination (#14)
    Add pagination to TopStationsFragment

--- app/src/main/java/net/moisesborges/ui/top/adapter/TopStationsAdapter.kt
+++ app/src/main/java/net/moisesborges/ui/top/adapter/TopStationsAdapter.kt
@@ -3,23 +3,26 @@ package net.moisesborges.ui.top.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import net.moisesborges.R
 import net.moisesborges.databinding.TopStationItemBinding
 import net.moisesborges.model.Station
+import net.moisesborges.ui.top.mvvm.PaginationDetector
 
 class TopStationsAdapter(
-    private val topStationItemViewModelFactory: TopStationItemViewModelFactory
+    private val topStationItemViewModelFactory: TopStationItemViewModelFactory,
+    private val paginationDetector: PaginationDetector
 ) : RecyclerView.Adapter<StationViewHolder>() {
 
-    private val stations = mutableListOf<Station>()
+    private var stations = listOf<Station>()
+    private var oldStations = listOf<Station>()
 
     fun setStations(stations: List<Station>) {
-        this.stations.apply {
-            clear()
-            addAll(stations)
-        }
-        notifyDataSetChanged()
+        this.oldStations = this.stations
+        this.stations = stations
+        val diffResult = DiffUtil.calculateDiff(TopStationsDiffCallback(this.oldStations, stations))
+        diffResult.dispatchUpdatesTo(this)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StationViewHolder {
@@ -36,6 +39,7 @@ class TopStationsAdapter(
     override fun onBindViewHolder(holder: StationViewHolder, position: Int) {
         val station = stations[position]
         holder.bind(topStationItemViewModelFactory.create(station))
+        paginationDetector.onItemDisplayed(position, stations.size)
     }
 }
 
@@ -46,5 +50,27 @@ class StationViewHolder(
     fun bind(viewModel: TopStationItemViewModel) {
         binding.viewModel = viewModel
         binding.executePendingBindings()
+    }
+}
+
+private class TopStationsDiffCallback(
+    private val oldStations: List<Station>,
+    private val newStations: List<Station>
+) : DiffUtil.Callback() {
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldStations[oldItemPosition].id == newStations[newItemPosition].id
+    }
+
+    override fun getOldListSize(): Int {
+        return oldStations.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newStations.size
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldStations[oldItemPosition] == newStations[newItemPosition]
     }
 }
